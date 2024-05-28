598aa66a1aa7d1b35151df6f2fa1354d0d40d12f
yehoni1 <48975285@est.ort.edu.ar>
2024-05-28 09:09:26 -0300

    cree contra
    contraseña

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public string Contra = "boca";
+    public InputField IngresaContra;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Contraseña()
+    {
+        if(IngresaContra.text == Contra)
+        {
+            Debug.Log("acceso garantizado");
+        }
+        else
+        {
+            Debug.Log("acceso denegado");
+        }
+    }
+}
